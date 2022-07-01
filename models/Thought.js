2092c4da2a69c682e161162a4765981654ffcc08
SafteyDav3 <talktomedw@gmail.com>
2022-07-01 14:22:24 -0500

    date format

---
+++ models/Thought.js
@@ -0,0 +1,4 @@
+const {Schema, model} = require('mongoose')
+
+
+model.exports = Thought;
--- server.js
+++ server.js
@@ -1,21 +1,25 @@
-// Bringing in 'dotenv' for credentials
-require('dotenv').config()
-console.log(process.env) // Remove before deployment
-
-
 const express = require("express");
 const mongoose = require("mongoose");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
 
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost:27017/Deep_Thought_DB", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+);
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(express.static('public'))
-app.use(require('./routes'))
+// Use this to log mongo queries being executed!
+mongoose.set("debug", true);
 
+app.use(require("./routes"));
 
-app.listen(PORT, ()=> {
-    console.log(`Server active @ http://localhost:${PORT}/`);
-})
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`)); // change log
