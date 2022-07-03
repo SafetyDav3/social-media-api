61df0ac082b3ddf56c991c0a905aa05e7d063ea4
SafteyDav3 <talktomedw@gmail.com>
2022-07-03 17:51:08 -0500

    refactored controllers

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,69 +1,42 @@
 const { Thought, User } = require("../models");
-
 const thoughtController = {
   createThought({ params, body }, res) {
     Thought.create(body)
       .then(({ _id }) => {
-        return Users.findOneAndUpdate(
-          {
-            _id: params.userId,
-          },
-          {
-            $push: {
-              Thought: _id,
-            },
-          },
-          {
-            new: true,
-          }
+        returnUsers.findOneAndUpdate(
+          { _id: params.userId },
+          { $push: { Thought: _id } },
+          { new: true }
         );
       })
       .then((dbThoughtData) => {
         if (!dbThoughtData) {
-          res.status(404).json({
-            message: "Thought not found",
-          });
+          res.status(404).json({ message: "Thought not found" });
           return;
         }
         res.json(dbThoughtData);
       })
       .catch((err) => res.json(err));
   },
-
   getAllThought(req, res) {
     Thought.find({})
-      .populate({
-        path: "reactions",
-        select: "-__v",
-      })
+      .populate({ path: "reactions", select: "-__v" })
       .select("-__v")
-      .sort({
-        _id: -1,
-      })
+      .sort({ _id: -1 })
       .then((dbThoughtData) => res.json(dbThoughtData))
       .catch((err) => {
         console.log(err);
         res.status(500).json(err);
       });
   },
-
   getThoughtById({ params }, res) {
-    Thought.findOne({
-      _id: params.id,
-    })
-      .populate({
-        path: "reactions",
-        select: "-__v",
-      })
+    Thought.findOne({ _id: params.id })
+      .populate({ path: "reactions", select: "-__v" })
       .select("-__v")
-      .sort({
-        _id: -1,
-      })
+      .sort({ _id: -1 })
       .then((dbThoughtData) => {
         if (!dbThoughtData) {
-          res.status(404).json({
-            message: "Thought not found",
-          });
+          res.status(404).json({ message: "Thought not found" });
           return;
         }
         res.json(dbThoughtData);
@@ -73,125 +46,70 @@ const thoughtController = {
         res.sendStatus(400);
       });
   },
-
   updateThought({ params, body }, res) {
-    Thought.findOneAndUpdate(
-      {
-        _id: params.id,
-      },
-      body,
-      {
-        new: true,
-        runValidators: true,
-      }
-    )
-      .populate({
-        path: "reactions",
-        select: "-__v",
-      })
+    Thought.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .populate({ path: "reactions", select: "-__v" })
       .select("-___v")
       .then((dbThoughtData) => {
         if (!dbThoughtData) {
-          res.status(404).json({
-            message: "Thought not found",
-          });
+          res.status(404).json({ message: "Thought not found" });
           return;
         }
         res.json(dbThoughtData);
       })
       .catch((err) => res.json(err));
   },
-
   deleteThought({ params }, res) {
-    Thought.findOneAndDelete({
-      _id: params.id,
-    })
+    Thought.findOneAndDelete({ _id: params.id })
       .then((dbThoughtData) => {
         if (!dbThoughtData) {
-          res.status(404).json({
-            message: "Thought not found",
-          });
+          res.status(404).json({ message: "Thought not found" });
           return;
         }
-        return User.findOneAndUpdate(
-          {
-            _id: parmas.userId,
-          },
-          {
-            $pull: {
-              Thought: params.Id,
-            },
-          },
-          {
-            new: true,
-          }
+        returnUser.findOneAndUpdate(
+          { _id: params.userId },
+          { $pull: { Thought: params.Id } },
+          { new: true }
         );
       })
       .then((dbUserData) => {
         if (!dbUserData) {
-          res.status(404).json({
-            message: "User not found",
-          });
+          res.status(404).json({ message: "User not found" });
           return;
         }
         res.json(dbUserData);
       })
       .catch((err) => res.status(400).json(err));
   },
-
   createReaction({ params, body }, res) {
     Thought.findOneAndUpdate(
-      {
-        _id: params.thoughtId,
-      },
-      {
-        $push: {
-          reactions: body,
-        },
-      },
-      {
-        new: true,
-        runValidators: true,
-      }
+      { _id: params.thoughtId },
+      { $push: { reactions: body } },
+      { new: true, runValidators: true }
     )
-      .populate({
-        path: "reactions",
-        select: "-__v",
-      })
+      .populate({ path: "reactions", select: "-__v" })
       .select("-__v")
       .then((dbThoughtData) => {
         if (!dbThoughtData) {
-          res.status(404).json({
-            message: "Thought not found",
-          });
+          res.status(404).json({ message: "Thought not found" });
           return;
         }
         res.json(dbThoughtData);
       })
       .catch((err) => res.status(400).json(err));
   },
-
   deleteReaction({ params }, res) {
     Thought.findOneAndUpdate(
-      {
-        _id: params.thoughtId,
-      },
-      {
-        $pull: {
-          reactions: {
-            reactionId: params.reactionId,
-          },
-        },
-      },
-      {
-        new: true,
-      }
+      { _id: params.thoughtId },
+      { $pull: { reactions: { reactionId: params.reactionId } } },
+      { new: true }
     )
       .then((dbThoughtData) => {
         if (!dbThoughtData) {
-          res.status(404).json({
-            message: "Thought not found",
-          });
+          res.status(404).json({ message: "Thought not found" });
           return;
         }
         res.json(dbThoughtData);
@@ -199,5 +117,4 @@ const thoughtController = {
       .catch((err) => res.status(400).json(err));
   },
 };
-
 module.exports = thoughtController;
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,51 +1,30 @@
 const { User } = require("../models");
-
 const userController = {
   createUser({ body }, res) {
     User.create(body)
       .then((dbUserData) => res.json(dbUserData))
       .catch((err) => res.status(400).json(err));
   },
-
   getAllUser(req, res) {
     User.find({})
-      .populate({
-        path: "Thought",
-        select: "-__v",
-      })
-      .populate({
-        path: "friends",
-        select: "-__v",
-      })
+      .populate({ path: "Thought", select: "-__v" })
+      .populate({ path: "friends", select: "-__v" })
       .select("-__v")
-      .sort({
-        _id: -1,
-      })
+      .sort({ _id: -1 })
       .then((dbUserData) => res.json(dbUserData))
       .catch((err) => {
         console.log(err);
         res.status(500).json(err);
       });
   },
-
   getUserById({ params }, res) {
-    User.findOne({
-      _id: params.id,
-    })
-      .populate({
-        path: "Thought",
-        select: "-__v",
-      })
-      .populate({
-        path: "friends",
-        select: "-__v",
-      })
+    User.findOne({ _id: params.id })
+      .populate({ path: "Thought", select: "-__v" })
+      .populate({ path: "friends", select: "-__v" })
       .select("-__v")
       .then((dbUserData) => {
         if (!dbUserData) {
-          res.status(404).json({
-            message: "User not found",
-          });
+          res.status(404).json({ message: "User not found" });
           return;
         }
         res.json(dbUserData);
@@ -55,42 +34,26 @@ const userController = {
         res.status(400).json(err);
       });
   },
-
   updateUser({ params, body }, res) {
-    User.findOneAndUpdate(
-      {
-        _id: params.id,
-      },
-      body,
-      {
-        new: true,
-        runValidators: true,
-      }
-    )
+    User.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
       .then((dbUserData) => {
         if (!dbUserData) {
-          res.status(404).json({
-            message: "User not found",
-          });
+          res.status(404).json({ message: "User not found" });
           return;
         }
         res.json(dbUserData);
       })
       .catch((err) => res.json(err));
   },
-
   deleteUser({ params }, res) {
-    Thought.deleteMany({
-      userId: params.id,
-    })
+    Thought.deleteMany({ userId: params.id })
       .then(() => {
-        User.findOneAndDelete({
-          userId: params.id,
-        }).then((dbUserData) => {
+        User.findOneAndDelete({ userId: params.id }).then((dbUserData) => {
           if (!dbUserData) {
-            res.status(404).json({
-              message: "User not found",
-            });
+            res.status(404).json({ message: "User not found" });
             return;
           }
           res.json(dbUserData);
@@ -98,62 +61,34 @@ const userController = {
       })
       .catch((err) => res.json(err));
   },
-
   addFriend({ params }, res) {
     User.findOneAndUpdate(
-      {
-        _id: params.id,
-      },
-      {
-        $push: {
-          friends: params.friendId,
-        },
-      },
-      {
-        new: true,
-      }
+      { _id: params.id },
+      { $push: { friends: params.friendId } },
+      { new: true }
     )
-      .populate({
-        path: "friends",
-        select: "-__v",
-      })
+      .populate({ path: "friends", select: "-__v" })
       .select("-__v")
       .then((dbUserData) => {
         if (!dbUserData) {
-          res.status(404).json({
-            message: "User not found",
-          });
+          res.status(404).json({ message: "User not found" });
           return;
         }
         res.json(dbUserData);
       })
       .catch((err) => res.json(err));
   },
-
   deleteFriend({ params }, res) {
     User.findOneAndUpdate(
-      {
-        _id: params.id,
-      },
-      {
-        $pull: {
-          friends: params.friendId,
-        },
-      },
-      {
-        new: true,
-      }
+      { _id: params.id },
+      { $pull: { friends: params.friendId } },
+      { new: true }
     )
-      .populate({
-        path: "friends",
-        select: "-__v",
-      })
+      .populate({ path: "friends", select: "-__v" })
       .select("-__v")
       .then((dbUserData) => {
         if (!dbUserData) {
-          res.status(404).json({
-            message: "User not found",
-          });
+          res.status(404).json({ message: "User not found" });
           return;
         }
         res.json(dbUserData);
@@ -161,5 +96,4 @@ const userController = {
       .catch((err) => res.status(400).json(err));
   },
 };
-
 module.exports = userController;
